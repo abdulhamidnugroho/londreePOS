ea5055b992371658bd7f793e4a4ab1e9dd921e80
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-06-02 10:23:42 +0700

    user factory (db seeding)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,8 +21,20 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
+        'telp' => $faker->unique()->randomNumber($nbDigits = 12),
+        'alamat' => $faker->address,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'type' => $faker->randomElement(['admin', 'operator']),
+        'id_owner' => $faker->unique()->randomNumber($nbDigits = 3),
+        'activation_code' => $faker->unique()->randomNumber($nbDigits = 6),
+        'trash' => $faker->randomElement([1, 0]),
+        'paket_akun_id' => $faker->randomElement(['free', 'biasa', 'spesial', 'logo', 'istimewa']),
+        'jml_transaksi' => $faker->unique()->randomNumber($nbDigits = 3),
+        'jml_kios' => $faker->randomDigit,
+        'pesan_antar' => $faker->randomElement([1, 0]),
+        'saldo' => $faker->randomElement([$faker->randomNumber($nbDigits = 5), 0]),
+        'referral' => $faker->word,
+        'email_verified_at' => now(),
         'remember_token' => Str::random(10),
     ];
 });
