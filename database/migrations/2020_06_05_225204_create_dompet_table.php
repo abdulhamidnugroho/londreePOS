47edb0f725f27aae09b05a29cd79ce1bca54f461
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-06-06 05:56:09 +0700

    ++ model, factory, migration, seeder Dompet

---
+++ database/migrations/2020_06_05_225204_create_dompet_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDompetTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dompet', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('tgl');
+            $table->integer('admin_id');
+            $table->string('remark');
+            $table->integer('nominal');
+            $table->enum('jenis', ['debit', 'credit']);
+            $table->float('saldo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dompet');
+    }
+}
